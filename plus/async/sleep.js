9679f4797b94da1fd9e7179e41eef75155de594a
Nagy Máté <Mate_Nagy2@epam.com>
2022-07-18 14:14:29 +0200

    Solved

--- plus/async/sleep.js
+++ plus/async/sleep.js
@@ -5,3 +5,20 @@
  * @returns {Promise} the promise which resolves when wait if over
  *                    or rejects if parameter is not correct
  */
+
+module.exports = async function sleep(sec) {
+
+    if (typeof sec !== "number") {
+        throw new Error("Not a valid paramter");
+    }
+        
+    if (sec > 10) {
+        sec = 10;
+    }else if (sec < 1) {
+        sec = 1;
+    }
+
+    time = sec * 1000;
+
+    return new Promise(resolve => setTimeout(resolve, time));
+}

86cef95e4d53ab0bc75b5d88ea634528f3d24f6a
Nagy Máté <Mate_Nagy2@epam.com>
2022-07-20 14:22:33 +0200

    HomePage és tesztje

--- module-3/pop/HomePage.js
+++ module-3/pop/HomePage.js
@@ -11,12 +11,31 @@ module.exports = class HomePage extends Layout {
     constructor() {
         super('/');
 
-        this.header = new Element('Header', by.css('header'));
-        this.header.addChildren(new Element("Logo", by.css('.header__logo')));
-        this.menuItems = new Elements("Menu Items", by.css('.top-navigation__item-link'));
+        this.header = new Element('Header', { css: 'header' });
+        this.header.addChildren(new Element("Logo", { css:'.header__logo'}));
+        this.menuItems = new Elements("Menu Items", { css: '.top-navigation__item-link'});
+
+        this.header.addChildren(new Element("Contact Us", { css: "cta-button-ui cta-button--envelope header__control"}));
+        this.header.addChildren(new Element("Search", { css: "header-search__button header__icon"}));
+        this.rolloverTiles = new Elements("Rollover Tiles", { css: "rollover-tiles-ui"});
+        this.partners = new Element("Partners", { css: "museo-slab"});
     }
 
-    get logo() {
-        return this.get('Logo');
+    getLogo(logo) {
+        return this.header.get(logo);
     }
+
+    getChildren() {
+        const children = Object.keys(this.header.children).join(", ");
+        return children;
+    }
+
+    getPartners(loc) {
+        if(loc === this.partners.locator.css){
+            return this.partners.locator.css;
+        } else {
+            throw new Error("No partners with this locator");
+        }
+    }
+
 }
--- module-3/test/pop/HomePage.spec.js
+++ module-3/test/pop/HomePage.spec.js
@@ -1,3 +1,60 @@
-describe('HomePage Class', () => {
+const HomePage = require("../../pop/HomePage");
+const Element = require("../../pop/Element");
+const Elements = require("../../pop/Elements");
+const expect = require('chai').expect;
+
+describe.only('HomePage Class', () => {
     // TODO: write tests
+    beforeEach(() => {
+        global.myHomePage = new HomePage;
+    });
+
+    afterEach(() => {
+        delete myHomePage;
+    })
+
+    it("Should be defined", () => {
+        expect(HomePage).to.be.instanceof(Function);
+    });
+
+    describe("Header", () => {
+        it("Should be an Element", () =>{
+            expect(myHomePage.header).to.be.instanceof(Element);
+        });
+
+        it("Should have children", () => {
+            const children = myHomePage.getChildren();
+            expect(children).to.be.equal("Logo, Contact Us, Search");
+        });
+    });
+
+    describe("Logo", () => {
+        it("Should give back logo", () =>{
+            expect(myHomePage.getLogo("Logo").locator().css).to.be.equal(".header__logo");
+        })
+
+        it("Should throw error if logo not found", () => {
+            expect(() => myHomePage.getLogo("logo")).to.throw();
+        })
+    })
+
+    describe("Rollover Tiles", () => {
+        it("Should be Elements", () => {
+            expect(myHomePage.rolloverTiles).to.be.instanceof(Elements);
+        });
+    });
+
+    describe("Partners", () => {
+        it("Shoud be defined", () => {
+            expect(myHomePage.partners).not.to.be.undefined;
+        });
+
+        it("Should give back partners' locator", () => {
+            expect(myHomePage.getPartners("museo-slab")).to.be.equal("museo-slab");
+        });
+
+        it("Should throw error for wrong locator", () => {
+            expect(() => myHomePage.getPartners("museo-snap")).to.throw();
+        })
+    });
 });

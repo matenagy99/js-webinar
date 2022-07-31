8df34942af6f038c1322db79401f2c0633fdacde
Nagy Máté <Mate_Nagy2@epam.com>
2022-07-31 16:46:07 +0200

    Solved

--- module-4/toCamelCase.js
+++ module-4/toCamelCase.js
@@ -5,3 +5,29 @@
  * @param {string} toConvert
  * @returns {string} camel-case string or empty string in other cases
  */
+
+module.exports = function toCamelCase(string) {
+    
+    if (typeof string === "number"){
+        return "";
+    }
+
+    let str = "";
+    
+    let subStrs = string.trim().split(/\s+/);
+    str += subStrs[0].toLowerCase();
+
+    for(let idx = 1; idx < subStrs.length; idx++){
+        str += subStrs[idx].slice(0, 1).toUpperCase() + subStrs[idx].trim().slice(1);
+    }
+
+    let specialChars = "<>@!#$%^&*()_+[]{}?:;|'\"\\,./~`-=";
+
+    for(let char = 0; char < specialChars.length; char++) {
+       if (str.includes(specialChars[char])) {
+            str = str.replace(specialChars[char], "");
+       }
+    }
+
+    return str;
+}

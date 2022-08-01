87da4c1d90332a7dffd73fdfb84368cdac932014
Nagy Máté <Mate_Nagy2@epam.com>
2022-08-01 09:06:26 +0200

    Solved

--- module-4/longestString.js
+++ module-4/longestString.js
@@ -6,3 +6,20 @@
  * @param {string[]} strings
  * @returns {string} longest string or empty string in other cases
  */
+
+module.exports = function longestString(strings) {
+
+    if (!(strings instanceof Array) || strings.length < 2) {
+        return "";
+    }
+
+    let max = "";
+
+    for (const word of strings.sort()) {
+        if (word.length > max.length) {
+            max = word;
+        }
+    }
+
+    return max;
+}

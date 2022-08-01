86e8f960323da35385a0aee20e6b7a6abb38b9c7
Nagy Máté <Mate_Nagy2@epam.com>
2022-08-01 11:16:33 +0200

    Solved

--- module-4/arrayEqual.js
+++ module-4/arrayEqual.js
@@ -11,3 +11,18 @@
  * @returns {boolean} true if the two arrays are equal,
  *                    false otherwise
  */
+
+module.exports = function arrayEqual(arr1, arr2) {
+
+    if (arr1.length !== arr2.length) {
+        console.warn(`Different lengths: ${arr1.length} !== ${arr2.length}`);
+    } else {
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                console.warn(`Different items: ${arr1[i]} !== ${arr2[i]}`)
+            } 
+        }
+    }
+    
+    return JSON.stringify(arr1) === JSON.stringify(arr2);
+}

ec875da71942c3627168052ff50c1486797536df
Nagy Máté <Mate_Nagy2@epam.com>
2022-08-01 10:19:38 +0200

    Nested-re még nem működik

--- module-4/arraySum.js
+++ module-4/arraySum.js
@@ -7,3 +7,37 @@
  * @param {Array} elements
  * @returns {number} summary of all integers or 0 in other cases
  */
+
+module.exports = function arraySum(arr, sum = 0) {
+    
+    if (!(arr instanceof Array) || arr.length === 0) {
+        return 0;
+    }
+
+    let sumItems = sum;
+
+    for (const item of arr) {
+        if (typeof item === "number") {
+            sumItems += item;
+        }
+
+        if (item instanceof Array) {
+            arraySum(item, sumItems);
+        }
+    }
+
+    /*
+    arr.forEach((element) => {
+        
+        if (typeof element === "number") {
+            sumItems += element;
+        }
+        
+        if (element instanceof Array) {
+            arraySum(element, sumItems);
+        }
+
+    });*/
+
+    return sumItems;
+}

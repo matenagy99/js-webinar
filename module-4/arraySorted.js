7c24bdf27d09c1e31551f09e4c89a689695eec2e
Nagy Máté <Mate_Nagy2@epam.com>
2022-08-01 11:02:03 +0200

    Solved

--- module-4/arraySorted.js
+++ module-4/arraySorted.js
@@ -12,3 +12,28 @@
  * @returns {boolean} true if the array is properly sorted,
  *                    false otherwise
  */
+
+module.exports = function arraySorted(arr, ignore) {
+    
+    let isSorted = true;
+
+    for (let idx = 0; idx < arr.length - 1; idx++) {
+
+        arr[idx] = arr[idx].toString().split(" ").join("").replace(/\s+/, "");
+        arr[idx + 1] = arr[idx + 1].toString().split(" ").join("").replace(/\s+/, "");
+
+        if (arr[idx].indexOf(ignore) !== -1) {
+            arr[idx] = arr[idx].replace(ignore, "");
+        }
+
+        if (arr[idx + 1].indexOf(ignore) !== -1) {
+            arr[idx + 1] = arr[idx + 1].replace(ignore, "");
+        }
+
+        if (arr[idx].toLowerCase() > arr[idx + 1].toLowerCase()) {
+            isSorted =  false;
+        }
+    }
+
+    return isSorted;
+}
